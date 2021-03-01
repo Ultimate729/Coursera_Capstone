911e0a68954f2e9e3f46360621d0f1f92fce4215
Ultimate729 <77784467+Ultimate729@users.noreply.github.com>
2021-03-01 09:11:15 +0000

    Assignment 1
    Added markdown cell describing what project includes
    Added pandas and numpy libraries
    Printed 'Hello Capstone Project Course!'

---
+++ Coursera Capstone - Assignment 1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This Project will be used to complete the Coursera Capstone Module
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
